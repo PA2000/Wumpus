958fd2b975e4c6e68ca1ebc2f41e9b4b441abb06
Charles Rodriguez <charliefrodriguez@gmail.com>
2020-12-11 15:40:56 -0500

    added in piece combination generator`:x

--- AVsearch.py
+++ AVsearch.py
@@ -6,7 +6,20 @@ import random
 import numpy as np
 from queue import PriorityQueue
 
+
+
+class piece_pacakge_info: 
+     
+     def __init__(self,piece_type,coors,obs): 
+        self.piece = peice 
+        sellf.piece_type = piece_type
+        self.coors = coors 
+        self.obs = obs 
+    
+
+
 #Tile Object for each tile in the gameboard
+
 class Tile:
     #load in images
 
@@ -126,6 +139,76 @@ class Tile:
         pygame.draw.line(screen, (0,0,0), [self.colval * h, self.rowval*h], [self.colval * h, self.rowval* h + h], 1)
         pygame.display.update()
         return
+
+   def map_check(piece_mappings,obs_locs): 
+        
+        for piece in piece_mappings:
+            for obs_pieces in obs_locs: 
+                if piece[1] == obs_pieces[1]: 
+                    for neighs in self.board[obs_pieces[1][0]][obs_pieces[1][1]]: 
+                        if piece 
+                       
+
+            
+
+
+
+   def piece_space_map(vec_piece,no_pieces,obs_loc): 
+        """" this function will take a set of pieces and map them to all posiblle lcoations on the board  """
+        open_spaces = [None]*0 
+        
+        for i in range(self.side): 
+            for j in range(self.side): 
+                if self.board[i][j].player == "neutral" and self.board[i][j].unit != "pit": 
+                    open_spaces.append([i,j])  
+        
+        """each for loop correpodns to the number of pieces that we have to generate spaces for. 
+           We are mapping a fector of pieces that are represented by chars to the generated positions.
+        """
+        
+        if no_pieces == 2:  
+            for op in open_spaces: 
+                for op2 in open_spaces: 
+                    if( map_check(obs_loc, [[vec_piece[0],op ], [vec_piece[1], op2]])):
+                        prob_func([[vec_piece[0],op ], [vec_piece[1], op2]])
+        
+        elif no_pieces == 3: 
+            for op in open_spaces: 
+                for op2 in open_spaces:
+                    for op3 in open_spaces:
+                        if( map_check(obs_loc, [[vec_piece[0],op ], [vec_piece[1], op2], [vec_piece[2],op3] ])):
+                            prob_func([[vec_piece[0],op ], [vec_piece[1], op2],[vec_piece[2],op3]])
+        
+        elif no_pieces == 4: 
+            for op in open_spaces: 
+                for op2 in open_spaces:
+                    for op3 in open_spaces:
+                        for op4 in open_spaces:
+                            if( map_check(obs_loc, [[vec_piece[0],op ], [vec_piece[1], op2], [vec_piece[2],op3], [vec_piece[3],op4] ])):
+                                prob_func([[vec_piece[0],op ], [vec_piece[1], op2],[vec_piece[2],op3],[vec_piece[3],op4]]) 
+        
+        elif no_pieces == 5: 
+            for op in open_spaces: 
+                for op2 in open_spaces:
+                      for op3 in open_spaces:
+                            for op4 in open_spaces:
+                                for op5 in open_spaces: 
+                                    if( map_check(obs_loc, [[vec_piece[0],op ], [vec_piece[1], op2], [vec_piece[2],op3], [vec_piece[3],op4], [vec_piece[4],op5] ])):
+                                        prob_func([[vec_piece[0],op ], [vec_piece[1], op2],[vec_piece[2],op3], [vec_piece[3],op4], [vec_piece[4],op5]]) 
+
+        else: 
+            for op in open_spaces: 
+                for op2 in open_spaces:
+                      for op3 in open_spaces:
+                            for op4 in open_spaces:
+                                for op5 in open_spaces:
+                                    for op6 in open_spaces:
+                                        if( map_check(obs_loc, [[vec_piece[0],op ], [vec_piece[1], op2], [vec_piece[2],op3], [vec_piece[3],op4], [vec_piece[4],op5],[vec_piece[5],op6]])):
+                                            prob_func([[vec_piece[0],op ], [vec_piece[1], op2], [vec_piece[2],op3], [vec_piece[3],op4], [vec_piece[4],op5],[vec_piece[5],op6]])
+
+
+    
+  
 
 #this function checks if the observation that a given tile receives is consistent with its neighboring pieces/pits.
     def filtering(self, screen, h, player):
